328ab8eb2b1969c521974a958e46da1dd1414e1c
nikkisengar <nikkisengar@gmail.com>
2018-06-19 14:06:27 +0530

    Opened in Oxygen.3A

---
+++ IOCProj1-BasicSetterInjacetion/src/com/nt/beans/WishMsgGenerator.java
@@ -0,0 +1,36 @@
+package com.nt.beans;
+
+import java.util.Date;
+
+//Bean Class with Business logic
+public class WishMsgGenerator {
+
+	//Declaring the Bean Property
+	private Date date;
+
+	//Setter Method of the bean property
+	public void setDate(Date date) {
+		System.out.println("Setter Method of the Bean Property.");
+		this.date = date;
+	}
+	
+	//generateWishMsg(-) method with business logic
+	public String generateWishMsg(String name){
+		
+		System.out.println("generateWishMsg(-) is called...");
+		//using local variable
+		int hours;
+		
+		//using the getHours() method of Date class
+		hours=date.getHours();
+		
+		if(hours<12)
+			return "GM :: "+name;
+		else if(hours<16)
+			return "GA :: "+name;
+		else if(hours<20)
+			return "GE :: "+name;
+		else
+			return "GN :: "+name;
+	}//Method Ends
+}//Class Ends
